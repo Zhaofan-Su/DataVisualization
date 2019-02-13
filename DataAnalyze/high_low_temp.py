d18825491e8a625727742023a48e888ea16df8a7
ZhaoFan-Su <1015655239@qq.com>
2019-02-13 13:45:53 +0800

    analyze the high and low temperatures

---
+++ DataAnalyze/high_low_temp.py
@@ -0,0 +1,37 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'sitka_weather_2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+# for index, column_header in enumerate(header_row):
+#    print(index, column_header)
+
+    # get the datas
+    dates = []
+    highs = []
+    lows = []
+    for row in reader:
+        date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(date)
+        highs.append(int(row[1]))
+        lows.append(int(row[3]))
+
+    # draw a figure to show the daily high and low temperatures
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
+
+    # some settings of the figure
+    plt.title("Daily high and low temperatures 2014", fontsize=24)
+    plt.xlabel('', fontsize=12)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (F)', fontsize=12)
+    plt.tick_params(axis='both', which='major', labelsize=12)
+
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.3)
+    plt.show()
